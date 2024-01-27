fd3344f9ff98153ec3cde8570e1a4758de305f8c
godaiflying <31681293+godaiflying@users.noreply.github.com>
2024-01-27 20:13:15 +1100

    Getting rating from a website using scraping

--- index.js
+++ index.js
@@ -7,5 +7,10 @@ const URL = "https://www.imdb.com/title/tt17632862/?ref_=ttls_li_tt";
 //nameless async function that runs normally
 (async () => {
         const response = await request(URL);
-        console.log(response);
+
+        
+        let $ = cheerio.load(response);
+        let rating = $('div[data-testid="hero-rating-bar__aggregate-rating__score"] > span').text();
+        console.log(rating);
+
 })()
