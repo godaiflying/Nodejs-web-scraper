6c7d0fbf9758da884efbc0dcb259f55046b8db94
godaiflying <31681293+godaiflying@users.noreply.github.com>
2024-01-29 23:17:19 +1100

    storing the data fetched in a array

--- index.js
+++ index.js
@@ -35,9 +35,19 @@ const URL = "https://www.imdb.com/title/tt17632862/?ref_=ttls_li_tt";
         let rating = $('div[data-testid="hero-rating-bar__aggregate-rating__score"] > span[class="sc-bde20123-1 cMEQkK"]').text();
         let poster =  $('div[class="ipc-media ipc-media--poster-27x40 ipc-image-media-ratio--poster-27x40 ipc-media--baseAlt ipc-media--poster-l ipc-poster__poster-image ipc-media__img"] > img').attr('src');
         let releaseDate = $('ul[class="ipc-inline-list ipc-inline-list--show-dividers sc-d8941411-2 cdJsTz baseAlt"] > li > a[href="/title/tt17632862/releaseinfo?ref_=tt_ov_rdat"]').text();
+
+        let creators = [];
+        $('div[class="ipc-metadata-list-item__content-container"] > ul[class="ipc-inline-list ipc-inline-list--show-dividers ipc-inline-list--inline ipc-metadata-list-item__list-content baseAlt"] > li').each((i, elm) => {
+                let creator = $(elm).text();
+
+                creators.push(creator);
+        });
+
         console.log("Title: "+ title);
         console.log("Rating: "+ rating);
         console.log("Poster: "+ poster);
         console.log("Release: "+ releaseDate);
+
+        console.log(creators);
 
 })()
